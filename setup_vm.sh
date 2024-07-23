ce80f0e415807a42b8c5e71de883b9ba4a98cbb8
J-sephB-lt-n <joseph.jazz.bolton@gmail.com>
2024-07-23 13:36:18 +0200

    fix(setup_vm.sh): fixed llama.cpp make for VM with cuda GPU

--- setup_vm.sh
+++ setup_vm.sh
@@ -7,8 +7,8 @@ git clone https://github.com/ggerganov/llama.cpp
 cd llama.cpp && make && cd ..
 
 # if cuda GPU available #
-# refer to: https://cloud.google.com/compute/docs/gpus/install-drivers-gpu
-cd llama.cpp && make make GGML_CUDA=1 && cd ..
+# useful reference: https://cloud.google.com/compute/docs/gpus/install-drivers-gpu
+cd llama.cpp && make GGML_CUDA=1 && cd ..
 
 mkdir llm_models
 wget -O llm_models/model.gguf 'https://huggingface.co/internlm/internlm2_5-7b-chat-gguf/resolve/main/internlm2_5-7b-chat-q5_k_m.gguf?download=true'
